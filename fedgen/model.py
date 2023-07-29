71f086413d4947a83e70bf88982a558fd0f8b50e
Ubuntu <ubuntu@ip-172-31-37-30.ap-south-1.compute.internal>
2023-07-29 07:33:51 +0000

    Initial Commit

---
+++ fedgen/model.py
@@ -0,0 +1,27 @@
+import torch, os
+from fas14mnet import Fas14MNet
+import packages.fedgen.fedgen.dataset as dataset
+from packages.fedgen.fedgen.dataset import FedDataset
+from opacus import PrivacyEngine
+from opacus.validators import ModuleValidator
+from opacus.utils.batch_memory_manager import BatchMemoryManager
+import torch.optim as optim
+import pandas as pd
+from torch.utils.data import DataLoader
+import pickle
+from packages.fedgen.fedgen.utils import PrivateModelBuilder
+from torchvision import models
+
+class EncryptData():
+
+    def __init__(self, categories=None, worker=None):
+        self.categories = categories
+        self.model = models.resnet18(pretrained=False)
+        num_features = self.model.fc.in_features
+        self.model.fc = torch.nn.Linear(num_features, self.categories)
+        self.worker = worker
+
+    def encrypt(self, trainDataLoader=None, epochs=None, MAX_GRAD_NORM=None, EPSILON=None, DELTA=None):
+        modelBuilder = PrivateModelBuilder(model=self.model, trainDataLoader=trainDataLoader)
+        self.model, _, _, _ = modelBuilder.privatization(MAX_GRAD_NORM=MAX_GRAD_NORM, EPSILON=EPSILON, DELTA=DELTA, EPOCHS=epochs)
+        torch.save(self.model.state_dict(), f'/home/ubuntu/kreedaAI/fedgen/models/encrypted_{self.worker}.pth')
